73e54ed5cb1ed5399e1322a33c138ab0bc027ea0
MiriamTayar <150809758+MiriamTayar@users.noreply.github.com>
2024-08-22 13:18:10 +0300

    Delete client/project directory

--- client/project/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import '../styles/HeaderStyle.css'
-import logo from '../images/1.png'
-export default function Header() {
-
-    return <div className="header">
-        <img src={logo} alt='logo' /> 
-        
-         </div>
-}
